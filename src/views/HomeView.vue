133f96d1176787ea740d988cdf21e5d701be7640
KevCast28 <kevinsn524@gmail.com>
2024-02-10 17:21:25 -0500

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   .prettierrc.json 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   env.d.ts 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   src/App.vue 	new file:   src/assets/base.css 	new file:   src/assets/logo.svg 	new file:   src/assets/main.css 	new file:   src/components/HelloWorld.vue 	new file:   src/components/TheWelcome.vue 	new file:   src/components/WelcomeItem.vue 	new file:   src/components/icons/IconCommunity.vue 	new file:   src/components/icons/IconDocumentation.vue 	new file:   src/components/icons/IconEcosystem.vue 	new file:   src/components/icons/IconSupport.vue 	new file:   src/components/icons/IconTooling.vue 	new file:   src/main.ts 	new file:   src/router/index.ts 	new file:   src/stores/counter.ts 	new file:   src/views/AboutView.vue 	new file:   src/views/HomeView.vue 	new file:   tsconfig.app.json 	new file:   tsconfig.json 	new file:   tsconfig.node.json 	new file:   vite.config.ts

---
+++ src/views/HomeView.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import {ref} from 'vue';
+import type {Ref} from 'vue';
+
+const name:Ref<string> = ref('Kevin');
+const age:Ref<number> = ref(23);
+const errors = ref([{
+  name: '',
+  age: ''
+}]);
+const error: Ref<string> = ref("#d03");
+
+const validation = () => {
+  if(name.value.length < 5) {
+    errors.value.push({name:'Error de edad'});
+  }
+}
+
+</script>
+
+<template>
+  <main>
+    <div>
+      <input @input="validation()" v-model="name" type="text">
+    </div>
+    <div>
+      <input v-model="name" type="text">
+    </div>
+    <div>
+      <input v-model="age" type="text">
+    </div>
+
+    <h1 v-bind:style="{'color':'#d03', 'background-color':'#cd3'}">{{ name }}</h1>
+    <h2 :class="{'error': age > 10}">{{ age }}</h2>
+    <h3>Errors:</h3>
+    <span v-for="(err, index)" in errors :key="index">
+    {{ error }}
+    </span>
+
+  </main>
+</template>
+<style scoped>
+  .error {
+    color: white;
+    background-color: red;
+    border: 1px solid white;
+  }
+</style>
